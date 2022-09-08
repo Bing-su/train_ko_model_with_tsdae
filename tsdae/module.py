854b64bc7a2c25f521a0826c4dd8aa0caff2f782
dowon <ks2515@naver.com>
2022-09-08 11:29:03 +0900

    fix: default params

--- tsdae/module.py
+++ tsdae/module.py
@@ -74,7 +74,7 @@ class KoTSDAEModule(pl.LightningModule):
         cycle_momentum = self.optimizer_name not in ("adan", "adapnm")
         scheduler = torch.optim.lr_scheduler.OneCycleLR(
             optimizer,
-            0.001,
+            0.01,
             total_steps=self.trainer.estimated_stepping_batches,
             cycle_momentum=cycle_momentum,
         )
