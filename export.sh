6d14641a5006e335e6b2595435be7b5576b47faa
dowon <ks2515@naver.com>
2022-09-14 09:22:03 +0900

    fix: .export -> export.sh

--- export.sh
+++ export.sh
@@ -1,7 +1,5 @@
 # wsl datasets cache 설정
 export HF_DATASETS_CACHE="/mnt/c/Users/smartmind/.cache/huggingface/datasets"
-
 # https://github.com/TimDettmers/bitsandbytes/issues/17
 export LD_LIBRARY_PATH=/usr/lib/wsl/lib
-
 export CUDA_HOME=/usr/local/cuda
